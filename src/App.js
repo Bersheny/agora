8b7ff4a1215515b0f93e625cd7476380bb4847cc
AurelieTorre <aurelie.torre@laposte.net>
2024-09-26 09:52:34 +0200

    there was a bug

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import Profile from './pages/Profile';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -11,12 +11,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-<<<<<<< HEAD
           <Route path="/profile/:userId" element={<Profile />} />
-=======
-          <Route path="/profile" element ={<Profile />} />
           <Route path="/login" element ={<Login />} />
->>>>>>> eb80c716e768d228a33c7495bc57ab7a49ddb143
         </Routes>
       </div>
     </Router>
